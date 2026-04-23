b6a88a180bbbfb3ec718cb28a3ecd203218da656
zDefcon <antholozadarequelme@gmail.com>
2026-04-23 16:43:50 -0500

    Agrega métodos para calcular y aplicar descuentos en el carrito
    Se implementan métodos para calcular el porcentaje de descuento y aplicarlo al total de la compra. Además, se ajusta la lógica de agregar productos y calcular el total para manejar correctamente la cantidad actual de productos en el carrito.

--- src/E2CarritoCompras/Carrito.java
+++ src/E2CarritoCompras/Carrito.java
@@ -5,6 +5,7 @@ public class Carrito {
     private Producto[] productos;
     private int cantActual;
     private int capacidadMax;
+    private final int porcentaje = 15;
 
     Carrito(int capacidadMax){
         this.capacidadMax = capacidadMax;
@@ -13,41 +14,48 @@ public class Carrito {
     }
 
     void agregarProducto(Producto producto){
-             if(cantActual < capacidadMax){
-                 productos[cantActual] = producto;
-                 cantActual++;
-             }
+        if(cantActual == capacidadMax){
+            System.out.println("Carrito Lleno, No se puede agregar el producto ["+ producto.getNombre()+"]");
+        }
 
-             if(cantActual == capacidadMax){
-                 System.out.println("Carrito Lleno, No se puede agregar el producto ["+ producto.getNombre()+"]");
-             }
+        if(cantActual < capacidadMax) {
+            productos[cantActual] = producto;
+            cantActual++;
+        }
     }
 
     double calcularTotal(){
         double total = 0.0;
-        for (int i = 0; i < capacidadMax; i++) {
-            total = total + productos[i].getPrecio();
+        for (int i = 0; i < cantActual; i++) {
+            if(productos[i] != null) {
+                total = total + productos[i].getPrecio();
+            }
         }
 
         return total;
     }
 
-    double calcularPorcentaje(int porcentaje ){
-        return calcularTotal()*(porcentaje/100);
+    double calcularPorcentaje(){
+        return (porcentaje*calcularTotal()/100);
     }
 
-    double aplicarDescuento(double porcentaje){
-        return calcularTotal() - (porcentaje * calcularTotal()/100);
+    double aplicarDescuento(){
+        return calcularTotal() - (calcularPorcentaje());
     }
 
-    String mostrarTicket(){
+    String mostrarTicket(boolean des){
         StringBuilder sbInfo = new StringBuilder();
         sbInfo.append("********** TICKET DE COMPRA **********\n");
         for (int i = 0; i < cantActual;i++) {
            sbInfo.append(productos[i].mostrarDetalle()).append("\n");
+
         }
 
         sbInfo.append("Total A Pagar: " + calcularTotal()).append("\n");
+
+        if(des == true){
+          sbInfo.append("Descuento Total: " + aplicarDescuento()).append("\n");
+        }
 
         return sbInfo.toString();
     }
