87a57df27ac62791d343b513af9916593ad2f2b6
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-24 20:02:40 -0500

    Agrega clase Vendedor para gestionar datos de vendedores
    Se crea la clase Vendedor que extiende de Empleado, incluyendo atributos para la comisión por venta y las ventas realizadas. Se implementa el método RegistrarVentas y se sobrescribe el método calcularPago para calcular el salario total basado en las ventas.

---
+++ src/E3SistemaEmpleados/Vendedor.java
@@ -0,0 +1,23 @@
+﻿package E3SistemaEmpleados;
+
+public class Vendedor extends Empleado{
+    private double comisionPorVenta;
+    private int ventasRealizadas;
+
+    public Vendedor(String nombre, double salario, double comisionPorVenta){
+        super(nombre, salario);
+        this.comisionPorVenta = comisionPorVenta;
+        ventasRealizadas = 0;
+    }
+
+    void RegistrarVentas(){
+        ventasRealizadas++;
+    }
+
+    @Override
+    public double calcularPago(){
+        return salarioBase + (comisionPorVenta * ventasRealizadas);
+    }
+
+
+}
