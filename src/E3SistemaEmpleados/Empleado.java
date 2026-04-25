4d45416b88ba33237612f4cbf45801cbf44c1846
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-24 19:41:20 -0500

    Agrega clase Empleado para gestionar datos de empleados
    Se crea la clase Empleado que incluye atributos para el nombre y salario base.
    Se implementan métodos para calcular el pago y mostrar los datos del empleado,
    facilitando la gestión de la información relacionada con los empleados en el sistema.

---
+++ src/E3SistemaEmpleados/Empleado.java
@@ -0,0 +1,22 @@
+﻿package E3SistemaEmpleados;
+
+public class Empleado {
+
+    protected String nombre;
+    protected Double salarioBase;
+
+    public Empleado(String nombre, Double salarioBase){
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+    }
+
+    double calcularPago(){
+        return salarioBase;
+    }
+
+    String mostrarDatos(){
+        return "Datos Personales Del Empleado: \n" +
+                 "Nombre: " + nombre + "\n" +
+                 "Salario: " + calcularPago() + "\n";
+    }
+}
