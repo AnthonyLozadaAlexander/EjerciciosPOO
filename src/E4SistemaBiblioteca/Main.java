1aeadbb2ba9a1ac93c191e5393e7250e71a7c16f
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 20:49:29 -0500

    Agrega libros a la inicialización en el método main
    Se añaden instancias de libros a la clase Main para permitir la gestión de préstamos en la aplicación. Esto facilita la interacción con los libros en el sistema.

--- src/E4SistemaBiblioteca/Main.java
+++ src/E4SistemaBiblioteca/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Lector Pepito = new Lector("Pepito");
         Lector Juanito = new Lector("Juanito");
+
+        Libro libroD = new Libro("Don Quijote", "Miguel Cervantes");
+        Libro libroS = new Libro("Shakespeare", "Libro");
+        Libro libroB = new Libro("Boku No Hero Academia", "Horikoshi");
+        Libro libroN = new Libro("Naruto", "Masashi Kishimoto");
+
+
+
     }
 }
