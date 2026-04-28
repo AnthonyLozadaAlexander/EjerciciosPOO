c0c5c3d0d407720391ce4ead310b0fe2101b51c8
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 20:58:47 -0500

    Agrega llamadas a tomarPrestado y mostrarLibros en main
    Se añaden invocaciones a los métodos tomarPrestado y mostrarLibros para los objetos Pepito y Juanito, permitiendo así la gestión de préstamos de libros y la visualización de los libros que poseen. Esto mejora la funcionalidad del programa al permitir interacciones más dinámicas con los objetos Libro.

--- src/E4SistemaBiblioteca/Main.java
+++ src/E4SistemaBiblioteca/Main.java
@@ -10,6 +10,14 @@ public class Main {
         Libro libroB = new Libro("Boku No Hero Academia", "Horikoshi");
         Libro libroN = new Libro("Naruto", "Masashi Kishimoto");
 
+        Pepito.tomarPrestado(libroD); // pepito tomar prestado el libro DonQuijote
+        Juanito.tomarPrestado(libroD); // Juanito intenta tomar prestado el libro DonQuijote
+        Pepito.tomarPrestado(libroS);
+        Pepito.tomarPrestado(libroB);
+        Pepito.tomarPrestado(libroN); // Error lleva mas libros de lo que pueden llevar [3]
+
+        Pepito.mostrarLibros(); // muestra los libros que tiene Pepito
+        Juanito.mostrarLibros(); // muestra los libros que tiene Juanito
 
 
     }
