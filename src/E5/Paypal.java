e203ce1990243da272aad08fed162a51e7eca433
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-28 01:15:17 -0500

    Agrega clase Paypal para procesamiento de pagos
    Se implementa la clase Paypal que extiende de MetodoPago,
    permitiendo el manejo de pagos a través de cuentas de Paypal.
    Incluye métodos para procesar pagos e imprimir recibos.

---
+++ src/E5/Paypal.java
@@ -0,0 +1,20 @@
+﻿package E5;
+
+public class Paypal extends MetodoPago{
+    private String correoElectronico;
+
+    public Paypal(String titular, String correoElectronico){
+        super(titular);
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Override
+    public void procesarPago(double monto) {
+        System.out.println("Conectando A La Cuenta Paypal de "+ correoElectronico +"\n" + "Pago de $" + monto + " procesando");
+    }
+
+    @Override
+    void imprimirRecibo(double monto) {
+        super.imprimirRecibo(monto);
+    }
+}
