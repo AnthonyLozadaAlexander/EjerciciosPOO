7ffd0393bff26a7ff84410827703fa15e305da02
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 21:29:12 -0500

    Elimina modificador de acceso en la clase Main

--- src/E4SistemaBiblioteca/Main.java
+++ src/E4SistemaBiblioteca/Main.java
@@ -1,6 +1,6 @@
-﻿package E4SistemaBiblioteca;
+package E4SistemaBiblioteca;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Lector Pepito = new Lector("Pepito");
         Lector Juanito = new Lector("Juanito");
