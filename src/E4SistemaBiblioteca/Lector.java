9f81ba7f37c5077566e96b0d40b6d463e686ad3e
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 20:36:23 -0500

    Agrega clase Lector para gestionar préstamos de libros
    Se implementa la clase Lector que permite a los usuarios tomar prestados libros del sistema. Incluye métodos para tomar prestado un libro y mostrar los libros actualmente prestados. Se establece un límite en la cantidad de libros que un lector puede tener prestados simultáneamente.

---
+++ src/E4SistemaBiblioteca/Lector.java
@@ -0,0 +1,44 @@
+﻿package E4SistemaBiblioteca;
+
+public class Lector {
+
+    private String nombre;
+    private Libro[] librosPrestados;
+    private int cantLibros;
+
+    public Lector(String nombre){
+        this.nombre = nombre;
+        this.librosPrestados = new Libro[3];
+        this.cantLibros = 0; // inicializa la cantLibros en 0
+    }
+
+    void tomarPrestado(Libro libro){
+        if(cantLibros == librosPrestados.length){
+            System.out.println("Lector no puede llevar mas libros");
+            return;
+        }
+
+        if(!(libro.getDisponible())){
+            System.out.println("El libro " + libro.getTitulo() + " no tiene disponibles");
+        }
+
+        libro.prestar();
+        librosPrestados[cantLibros] = libro;
+        cantLibros++;
+
+        System.out.println(nombre + " ha tomado prestado el libro: " + libro.getTitulo());
+    }
+
+    void mostrarLibros(){
+        if(cantLibros == 0){
+            System.out.println("No hay libros ingresados");
+        }
+        for (int i = 0; i < librosPrestados.length; i++) {
+        if(librosPrestados[i] != null){
+            System.out.println(librosPrestados[i].getTitulo());
+        }
+        }
+    }
+
+
+}
