bacd2ae5ed189eca9204bf4e515c2785307f755f
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-01 00:25:53 -0500

    Corrige método imprimirRecibo en clase Paypal Se elimina la sobreescritura del método imprimirRecibo y se llama directamente desde procesarPago para simplificar el flujo de procesamiento de pagos.

--- src/E5/Paypal.java
+++ src/E5/Paypal.java
@@ -1,4 +1,4 @@
-﻿package E5;
+package E5;
 
 public class Paypal extends MetodoPago{
     private String correoElectronico;
@@ -11,10 +11,6 @@ public class Paypal extends MetodoPago{
     @Override
     public void procesarPago(double monto) {
         System.out.println("Conectando A La Cuenta Paypal de "+ correoElectronico +"\n" + "Pago de $" + monto + " procesando");
-    }
-
-    @Override
-    void imprimirRecibo(double monto) {
-        super.imprimirRecibo(monto);
+        imprimirRecibo(monto);
     }
 }
